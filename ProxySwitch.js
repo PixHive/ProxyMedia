dbeafb8781f14fe6630fee15c5bbe95b9a1482f7
PixHive <99159783+PixHive@users.noreply.github.com>
2024-01-06 11:01:31 +0800

    Update ProxySwitch.js

--- ProxySwitch.js
+++ ProxySwitch.js
@@ -1,8 +1,8 @@
 var d = new Date();
 var h = d.getHours();
 
-const ProxyDay   = ' 利刃';
-const ProxyNight = ' 搬瓦工';
+const ProxyDay   = '利刃';
+const ProxyNight = '搬瓦工';
 
 if (h >= 21 || h <1) {
     var ProxyNewPolicy = ProxyNight;
@@ -10,8 +10,8 @@ if (h >= 21 || h <1) {
     var ProxyNewPolicy = ProxyDay;
 }
 
-$surge.setSelectGroupPolicy(' 🐈 自建节点', ProxyNewPolicy);
-$notification.post("Script called", "ProxySwitch.js", "' 🐈 自建节点' switched to '" + ProxyNewPolicy + "'");
-console.log("ProxySwitch.js script: ' 🐈 自建节点' switched to '" + ProxyNewPolicy + "'");
+$surge.setSelectGroupPolicy('🐈 自建节点', ProxyNewPolicy);
+$notification.post("Script called", "ProxySwitch.js", "'🐈 自建节点' switched to '" + ProxyNewPolicy + "'");
+console.log("ProxySwitch.js script: '🐈 自建节点' switched to '" + ProxyNewPolicy + "'");
 
 $done();
